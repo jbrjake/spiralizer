22b4cea72c691b7cf8d76d40b1474346d5d96cdc
Jon Rubin <jb.rubin@gmail.com>
2016-03-26 10:34:54 -0700

    Add a project stub.

---
+++ Spiralizer/Spiralizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spiralizer
+//
+//  Created by Jonathon Rubin on 3/26/16.
+//  Copyright © 2016 Jonathon Rubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
