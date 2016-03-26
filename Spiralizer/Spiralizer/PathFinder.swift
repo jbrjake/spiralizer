2cabd290d55ef46fe52f98c18af51f5802ff689d
Jon Rubin <jb.rubin@gmail.com>
2016-03-26 13:31:46 -0700

    Fills in what I remember of the implementation from yesterday.

--- Spiralizer/Spiralizer/PathFinder.swift
+++ Spiralizer/Spiralizer/PathFinder.swift
@@ -31,11 +31,38 @@ public class PathFinder {
     }
     
     private func generateSpiralPath() {
+        respiralize(withWidth: self.width+1, andHeight: self.height+1, andOffset: 0)
+    }
+    
+    private func respiralize(withWidth width :Int, andHeight height :Int, andOffset offset :Int) {
+        
+        if offset >= self.width / 2 {
+            return
+        }
+
+        if offset >= self.height / 2 {
+            return
+        }
+        
+        for i in offset...width-offset-1 {
+            pathPoints.append(PathPoint(x: i, y: offset))
+        }
+        
+        for i in offset+1...height-offset-1 {
+            pathPoints.append(PathPoint(x: width-offset-1, y: i))
+        }
+        
+        for i in (offset...width-offset-1).reverse() {
+            pathPoints.append(PathPoint(x: i, y: height-offset))
+        }
+        
+        for i in (offset...height-1).reverse() {
+            pathPoints.append(PathPoint(x: offset, y: i))
+        }
+        
+        respiralize(withWidth: width, andHeight: height, andOffset: offset+1)
         
     }
     
-    private func respiralize(withWidth width :Int, withHeight height :Int) {
-        
-    }
     
 }
