f6d5f1f2700332b197012571826cd4a24290753f
Jon Rubin <jb.rubin@gmail.com>
2016-03-26 10:37:28 -0700

    Stubs out the PathFinder class, which will be responsible for generating the sequence of coordinates to move through to draw the spiral.

---
+++ Spiralizer/Spiralizer/PathFinder.swift
@@ -0,0 +1,13 @@
+//
+//  PathFinder.swift
+//  Spiralizer
+//
+//  Created by Jonathon Rubin on 3/26/16.
+//  Copyright © 2016 Jonathon Rubin. All rights reserved.
+//
+
+import Foundation
+
+class PathFinder {
+    
+}
