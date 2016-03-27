7b652ffd9da3d1fce8bbf651ff6bd6133e384770
Jon Rubin <jb.rubin@gmail.com>
2016-03-26 18:44:18 -0700

    Adds a simple 3x3 grid test case.

--- Spiralizer/SpiralizerTests/SpiralizerTests.swift
+++ Spiralizer/SpiralizerTests/SpiralizerTests.swift
@@ -13,17 +13,41 @@ class SpiralizerTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+    func testSpiral3x3() {
+        let pathFinder = PathFinder(withGridWidth: 3, andGridHeight: 3)
+        
+        let path = pathFinder.pathPoints
+        
+        XCTAssert(path.count == 3 * 3, "There should be a point for each square in the grid")
+        
+        /*      0   1   2
+              -------------
+            0 | 0 | 1 | 2 |
+              -------------
+            1 | 7 | 8 | 3 |
+              -------------
+            2 | 6 | 5 | 4 |
+              -------------
+        */
+        
+        XCTAssert(path[0] == PathPoint(x: 0, y: 0), "The 1st point should be 0,0")
+        XCTAssert(path[1] == PathPoint(x: 1, y: 0), "The 2nd point should be 1,0")
+        XCTAssert(path[2] == PathPoint(x: 2, y: 0), "The 3rd point should be 2,0")
+        XCTAssert(path[3] == PathPoint(x: 2, y: 1), "The 4th point should be 2,1")
+        XCTAssert(path[4] == PathPoint(x: 2, y: 2), "The 5th point should be 2,2")
+        XCTAssert(path[5] == PathPoint(x: 1, y: 2), "The 6th point should be 1,2")
+        XCTAssert(path[6] == PathPoint(x: 0, y: 2), "The 7th point should be 0,2")
+        XCTAssert(path[7] == PathPoint(x: 0, y: 1), "The 8th point should be 0,1")
+        XCTAssert(path[8] == PathPoint(x: 1, y: 1), "The 9th point should be 1,1")
+        
+        
+    }
     }
     
     func testPerformanceExample() {
