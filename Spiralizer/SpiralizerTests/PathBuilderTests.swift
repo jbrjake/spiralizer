506b4e5c9918b450a17a95aab55945f0443961e1
Jon Rubin <jb.rubin@gmail.com>
2016-03-27 11:00:20 -0700

    Adds a new test file for the path builder class and renames the existing one to PathFinderTests

---
+++ Spiralizer/SpiralizerTests/PathBuilderTests.swift
@@ -0,0 +1,35 @@
+//
+//  PathBuilderTests.swift
+//  Spiralizer
+//
+//  Created by Jonathon Rubin on 3/27/16.
+//  Copyright © 2016 Jonathon Rubin. All rights reserved.
+//
+
+import XCTest
+
+class PathBuilderTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
